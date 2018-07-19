da5bc9f211196b016b9f7e4d38b50afdeae19151
MakzZi <makzzi777@gmail.com>
2018-07-19 21:43:51 +0300

    initial program

---
+++ first_C_program/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  first_C_program
+//
+//  Created by Zlodeev on 7/19/18.
+//  Copyright © 2018 makzzi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Just one small step for coders. One giant leap for programmers!\n");
+    return 0;
+}
