8678eeaba3530cc3e3d4d5c0fe17cfd38372b685
MakzZi <makzzi777@gmail.com>
2018-07-25 21:22:19 +0300

    create password

--- first_C_program/main.c
+++ first_C_program/main.c
@@ -8,23 +8,53 @@
 
 #include <stdio.h>
 #include <string.h>
-#define MAX_LENGTH 256
-
+#include <ctype.h>
+#define MAX_LENGTH 128
+#define PASS_LENGTH 8
 
 int main() {
-    printf("Enter string: ");
     char buffer[MAX_LENGTH];
+    int upper;
+    int lower;
+    int digit;
     int i = -1;
-    do {
-        i++;
-        buffer[i] = getchar();
-    } while (buffer[i] != '\n');
-    buffer[i] = '\0';
-    i++;
-    char string[i];
-    for (int j = 0; j < i; j++) {
-        string[j] = buffer[j];
+    while (1) {
+        upper = lower = digit = 0;
+        i = -1;
+        printf("Create new password (maximum length %d characters): ", MAX_LENGTH);
+        do {
+            i++;
+            buffer[i] = getchar();
+            if (isdigit(buffer[i])) {
+                digit = 1;
+            }
+            if (islower(buffer[i])) {
+                lower = 1;
+            }
+            if (isupper(buffer[i])) {
+                upper = 1;
+            }
+        } while (buffer[i] != '\n');
+        buffer[i] = '\0';
+        if (strlen(buffer) >= PASS_LENGTH) {
+            if (digit && upper && lower) {
+                break;
+            }
+            else {
+                if (!digit) {
+                    printf("The password must have at least one digit!\n");
+                }
+                if (!lower || !upper) {
+                    printf("The password must contain upper and lower case letters!\n");
+                }
+            }
+        }
+        else {
+            printf("The password must be greater than or equal to %d characters!\n", PASS_LENGTH);
+        }
     }
-    printf("string = %s\n", string);
+    char string[i+1];
+    strcpy(string, buffer);
+    printf("Password is confirmed!\n");
     return 0;
 }
