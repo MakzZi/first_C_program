9284d9b411c22ef3c5809c2950d2428bf1f03be2
MakzZi <makzzi777@gmail.com>
2018-07-31 20:23:07 +0300

    creation of an integer dynamic array, printing on the console of the smalletst, biggest and average values in the array

--- first_C_program/main.c
+++ first_C_program/main.c
@@ -9,34 +9,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 20
-#define RANGE 100
+#define SIZE 50
+#define RANGE 777
 
 int main() {
-    srand(time(NULL));
-    int nums[SIZE];
-    int i, bubble, swiped, buffer;
-    printf("UNSORTED: ");
-    for (i = 0; i < SIZE; i++) {
-        nums[i] = (rand() % RANGE) + 1;
-        printf("%d;  ", nums[i]);
-    }
-    for (bubble = 0; bubble < SIZE-1; bubble++) {
-        swiped = 0;
-        for (i = bubble; i < SIZE; i++) {
-            if (nums[bubble] > nums[i]) {
-                buffer = nums[bubble];
-                nums[bubble] = nums[i];
-                nums[i] = buffer;
-                swiped = 1;
-            }
+    if(SIZE > 0) {
+        srand(time(NULL));
+        int* nums = (int*) malloc(SIZE * sizeof(int));
+        if (!nums) {
+            printf("error: not enough memory!\n");
+            exit(1);
         }
-        if (!swiped) break;
+        int smallest, biggest, i;
+        double sum;
+        nums[0] = (rand() % RANGE) + 1;
+        smallest = biggest = sum = nums[0];
+        printf("nums: %d; ", nums[0]);
+        for (i = 1; i < SIZE; i++) {
+            nums[i] = (rand() % RANGE) + 1;
+            if (nums[i] < smallest) {
+                smallest = nums[i];
+            }
+            else if (nums[i] > biggest) {
+                biggest = nums[i];
+            }
+            sum += nums[i];
+            printf("  %d;", nums[i]);
+        }
+        printf("...\nsmallest = %i\nbiggest = %i\naverage = %.3lf\n", smallest, biggest, sum/SIZE);
+        free(nums);
     }
-    printf("\n  SORTED: ");
-    for (i = 0; i < SIZE-1; i++) {
-        printf("%d;  ", nums[i]);
-    }
-    printf("%d.\n", nums[SIZE-1]);
     return 0;
 }
