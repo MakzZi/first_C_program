5efdd68b39d05b0df371515926d8c67a3fab87fd
MakzZi <makzzi777@gmail.com>
2018-08-04 14:59:41 +0300

    Added .gitignore and removed ivanov.*

--- first_C_program/ivanov.c
+++
@@ -1,9 +0,0 @@
-//
-//  ivanov.c
-//  first_C_program
-//
-//  Created by Zlodeev on 7/20/18.
-//  Copyright © 2018 makzzi. All rights reserved.
-//
-
-#include "ivanov.h"
---
+++ first_C_program/student.h
@@ -0,0 +1,13 @@
+//
+//  student.h
+//  first_C_program
+//
+//  Created by Zlodeev on 8/4/18.
+//  Copyright © 2018 makzzi. All rights reserved.
+//
+
+#ifndef student_h
+#define student_h
+
+
+#endif /* student_h */
